b50558b1e0b852dac14309fe5c59d05bdc086108
Oudmane <ayoub@oudmane.me>
2015-12-30 11:30:23 +0000

    a Commit

---
+++ src/OudyAPI.js
@@ -0,0 +1,59 @@
+OudyAPI = {
+    url: {
+        socket: '',
+        ajax: ''
+    },
+    busy: false,
+    socket: null,
+    ajax: null,
+    data: {},
+    connect: function() {
+        this.socket = new WebSocket(this.url.socket);
+        this.socket.onopen = this.events.open;
+        this.socket.onerror = this.events.error;
+        this.socket.onclose = this.events.close;
+        this.socket.onmessage = this.events.message;
+    },
+    events: {
+        open: function(event) {
+            
+        },
+        close: function(event) {
+            
+        },
+        message: function(event) {
+            
+        },
+        error: function(event) {
+            
+        }
+    },
+    send: function(request) {
+        if(this.ajax && this.ajax.readyState != 4) {
+            $.when(this.ajax).done(function() {
+                OudyAPI.send(request);
+            });
+        } else {
+            request.xhrFields = {
+                withCredentials: true
+            };
+            this.ajax = $.ajax(
+                this.url.ajax+request.uri+(request.render ? '?render='+request.render : ''),
+                request
+            );
+        }
+    },
+    parse: function(uri) {
+        var a =  document.createElement('a');
+        a.href = url;
+        return {
+            uri: a.pathname,
+            query: a.search.replace(/^\?/, '')
+        };
+    },
+    get: function(request) {
+        request.data = $.extend({}, OudyAPI.data, request.data);
+        request.method = 'GET';
+        this.send(request);
+    }
+};
