3887ec3f8fd6e1554f59759f997c66559e541a98
Ayoub Oudmane <ayoub@oudmane.me>
2016-05-21 16:28:27 +0100

    Added Bower

--- src/OudyAPI.js
+++ src/OudyAPI.js
@@ -1,61 +1,65 @@
-OudyAPI = {
+var OudyAPI = {
     url: {
-        socket: '',
+        socket: null,
         ajax: ''
     },
     busy: false,
     socket: null,
     ajax: null,
-    data: {},
-    connect: function() {
-        this.socket = new WebSocket(this.url.socket);
-        this.socket.onopen = this.events.open;
-        this.socket.onerror = this.events.error;
-        this.socket.onclose = this.events.close;
-        this.socket.onmessage = this.events.message;
-    },
     events: {
-        open: function(event) {
-            
-        },
-        close: function(event) {
-            
-        },
-        message: function(event) {
-            
-        },
-        error: function(event) {
-            
-        }
+        open: function(event) {},
+        close: function(event) {},
+        message: function(event) {},
+        error: function(event) {},
+        beforeSend: function(request) {}
     },
+    callbacks: {},
     send: function(request) {
-        if(this.ajax && this.ajax.readyState != 4) {
+        if(this.socket && this.socket.readyState == 1) {
+            if(request.success && !request.id) {
+                request.id = this.getID();
+                this.callbacks[request.id] = request.success;
+            }
+            this.events.beforeSend(request);
+            if(typeof request.beforeSend  === 'function')
+                request.beforeSend();
+            this.socket.send(JSON.stringify(request));
+        } else if(this.ajax && this.ajax.readyState != 4) {
             $this = this;
             $.when(this.ajax).done(function() {
                 $this.send(request);
             });
         } else {
-            request.xhrFields = {
-                withCredentials: true
-            };
             this.ajax = $.ajax(
                 this.url.ajax+request.uri,
                 request
             );
         }
     },
-    parse: function(uri) {
-        var a =  document.createElement('a');
-        a.href = url;
-        return {
-            uri: a.pathname,
-            query: a.search.replace(/^\?/, '')
+    getID: function() {
+        var text = '',
+            possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for(var i=0; i < 16; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    },
+    connect: function() {
+        this.socket = new WebSocket(this.url.socket);
+        this.socket.$this = this;
+        this.socket.onopen = this.events.open;
+        this.socket.onerror = this.events.error;
+        this.socket.onclose = this.events.close;
+        this.socket.onmessage = function(response) {
+            data = JSON.parse(response.data);
+            this.$this.events.message(data);
+            if(this.$this.callbacks.hasOwnProperty(data.id))
+                this.$this.callbacks[data.id](data.response, data.id);
         };
     },
-    get: function(request) {
-        $this = this;
-        request.data = $.extend({}, $this.data, request.data);
-        request.method = 'GET';
-        this.send(request);
+    init: function() {
+        if(("WebSocket" in window && window.WebSocket != undefined) || ("MozWebSocket" in window))
+            this.connect();
     }
 };
