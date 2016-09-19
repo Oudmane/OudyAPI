0d5eef21719eb8479528c429b179ad394da0f087
Ayoub Oudmane <ayoub@oudmane.me>
2016-09-19 23:34:45 +0100

    Rewrite with better Events

--- OudyAPI.js
+++ OudyAPI.js
@@ -1,5 +1,6 @@
 var jQuery = require('jquery'),
-    OudyAPI;
+    OudyAPI,
+    events = jQuery({});
 
 jQuery.fn.URI = function() {
     switch(this.prop('tagName')) {
@@ -28,56 +29,81 @@ module.exports = {
         socket: '',
         ajax: ''
     },
-    busy: false,
     socket: null,
     ajax: null,
-    events: {
-        open: function(event) {},
-        close: function(event) {},
-        message: function(event) {},
-        error: function(event) {},
-        beforeSend: function(request) {},
-        success: function(request) {}
-    },
     callbacks: {},
-    interface: 'api',
     send: function(request) {
-        if(!request.interface)
-            request.interface = this.interface;
-        this.events.beforeSend(request);
-        if(this.socket && this.socket.readyState == 1) {
-            if(request.success && !request.id && request.success) {
-                request.id = this.getID();
-                this.callbacks[request.id] = request.success;
-            }
-            if(typeof request.beforeSend  === 'function')
-                request.beforeSend();
-            this.socket.send(JSON.stringify(request));
-        } else {
-            request.headers = jQuery.extend(
-                {},
-                {
-                    Interface: request.interface
-                },
-                request.headers || {}
-            );
-            if(!request.success)
-                request.success = this.callbacks[request.interface];
-            if(this.ajax && this.ajax.readyState != 4) {
-                jQuery.when(this.ajax).done(function() {
-                    OudyAPI.send(request);
-                });
+        if(this.isReady()) {
+            if(request.interface)
+                OudyAPI.trigger('before:'+request.interface, request);
+            this.trigger('before', request);
+            if(this.socket) {
+                this.trigger('before:socket', request);
+                if(request.success && !request.interface) {
+                    request.id = this.generateID();
+                    this.one('success:'+request.id, function(event, response) {
+                        request.success(response.response);
+                    });
+                }
+                this.socket.send(JSON.stringify(request));
             } else {
-                this.ajax = jQuery.ajax(
-                    this.url.ajax+request.uri,
-                    request
-                ).done(function(response) {
-                    OudyAPI.events.success(response);
+                this.ajax = jQuery.ajax({
+                    url: request.uri,
+                    method: (request.method || 'GET'),
+                    data: (request.data || false),
+                    headers: jQuery.extend(
+                        request.interface ? {
+                            Interface: request.interface
+                        } : {},
+                        request.headers || {}
+                    ),
+                    success: function(response) {
+                        if(request.success)
+                            request.success(response);
+                        else if(request.interface)
+                            OudyAPI.trigger('success:'+request.interface, {
+                                response: response
+                            });
+                        OudyAPI.trigger('success', {
+                            response: response
+                        });
+                    },
+                    error: function(response) {
+                        if(request.error)
+                            request.error(response);
+                        else if(request.interface)
+                            OudyAPI.trigger('error:'+request.interface, {
+                                response: response
+                            });
+                        OudyAPI.trigger('error', {
+                            response: response
+                        });
+                    },
+                    complete: function(response) {
+                        if(request.complete)
+                            request.complete(response);
+                        else if(request.interface)
+                            OudyAPI.trigger('complete:'+request.interface, {
+                                response: response
+                            });
+                        OudyAPI.trigger('complete', {
+                            response: response
+                        });
+                    }
                 });
             }
-        }
+        } else
+            jQuery.when(this.ajax).done(function() {
+                OudyAPI.send(request);
+            });
     },
-    getID: function() {
+    isReady: function() {
+        if(this.ajax && [XMLHttpRequest.UNSENT, XMLHttpRequest.DONE].indexOf(this.ajax.readyState) != -1)
+            if(this.socket && this.socket.readyState != WebSocket.OPEN)
+                return false;
+        return true;
+    },
+    generateID: function() {
         var text = '',
             possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
@@ -87,24 +113,34 @@ module.exports = {
         return text;
     },
     connect: function() {
+        if(!this.url.socket)
+            this.url.socket = location.protocol.replace('http', 'ws')+'//'+location.hostname;
         this.socket = new WebSocket(this.url.socket);
-        this.socket.onopen = this.events.open;
-        this.socket.onerror = this.events.error;
-        this.socket.onclose = this.events.close;
+        this.socket.onopen = function(event) {
+            OudyAPI.trigger('socket.open', event);
+        };
+        this.socket.onerror = function(event) {
+            OudyAPI.trigger('socket.error', event);
+        };
+        this.socket.onclose = function(event) {
+            OudyAPI.trigger('socket.close', event);
+        };
         this.socket.onmessage = function(response) {
-            data = JSON.parse(response.data);
-            OudyAPI.events.message(data);
-            if(data.interface)
-                OudyAPI.callbacks[data.interface](data.response, data.id);
-            else if(OudyAPI.callbacks.hasOwnProperty(data.id)) {
-                OudyAPI.callbacks[data.id](data.response, data.id);
-                delete OudyAPI.callbacks[data.id];
-            }
+            var data = JSON.parse(response.data),
+                id = data.interface | data.id;
+            console.log(data.response);
+            OudyAPI.trigger('success.'+id, data);
+            OudyAPI.trigger('success', data);
+            OudyAPI.trigger('complete', data);
         };
     },
-    init: function() {
+    init: function(socket) {
         OudyAPI = this;
-        if(("WebSocket" in window && window.WebSocket != undefined) || ("MozWebSocket" in window))
+        if(socket && (("WebSocket" in window && window.WebSocket != undefined) || ("MozWebSocket" in window)))
             this.connect();
-    }
+        this.trigger('init', socket);
+    },
+    on: events.on.bind(events),
+    one: events.one.bind(events),
+    trigger: events.trigger.bind(events)
 };
